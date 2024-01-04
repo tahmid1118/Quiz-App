a991ba0fd3a75ad04c3146c8874b92dd343a0b75
tahmid1118 <tahmidshahriar.bd@gmail.com>
2024-01-04 16:45:44 +0600

    questions set

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,117 @@
+package com.example.quizapp
+
+import kotlin.collections.ArrayList
+
+object Constants {
+    fun getQuestions(): ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+        val que1 = Question(
+            1,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_bangladesh,
+            "Bangladesh",
+            "Australia",
+            "Armenia",
+            "Austria",
+            1
+        )
+        questionsList.add(que1)
+
+        val que2 = Question(
+            2,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Angola",
+            "Austria",
+            "Australia",
+            "Armenia",
+            2
+        )
+        questionsList.add(que2)
+
+        val que3 = Question(
+            3,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Belarus",
+            "Belize",
+            "Brunei",
+            "Brazil",
+            4
+        )
+        questionsList.add(que3)
+
+        val que4 = Question(
+            4,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Bahamas",
+            "Belgium",
+            "Barbados",
+            "Belize",
+            2
+        )
+        questionsList.add(que4)
+
+        val que5 = Question(
+            5,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "Gabon",
+            "France",
+            "Fiji",
+            "Finland",
+            3
+        )
+        questionsList.add(que5)
+
+        val que6 = Question(
+            6,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "Germany",
+            "Georgia",
+            "Greece",
+            "None of these",
+            1
+        )
+        questionsList.add(que6)
+
+        val que7 = Question(
+            1,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Dominica",
+            "Egypt",
+            "Denmark",
+            "Ethiopia",
+            3
+        )
+        questionsList.add(que7)
+
+        val que8 = Question(
+            8,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "Australia",
+            "Armenia",
+            "Austria",
+            1
+        )
+        questionsList.add(que8)
+
+        val que9 = Question(
+            9,
+            "Which country does the flag belong to?",
+            R.drawable.ic_flag_of_new_zealand,
+            "New Zealand",
+            "Jordan",
+            "Sudan",
+            "Palestine",
+            1
+        )
+        questionsList.add(que9)
+        return questionsList
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,69 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.util.ArrayList
+
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    private  var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+    private var btnSubmit: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+        btnSubmit = findViewById(R.id.btn_submit)
+        mQuestionsList = Constants.getQuestions()
+        setQuestion()
+
+    }
+
+    private fun setQuestion() {
+        val question: Question = mQuestionsList!![mCurrentPosition - 1]
+        ivImage?.setImageResource(question.image)
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition / ${progressBar?.max}"
+        tvQuestion?.text = question.quention
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+
+        if(mCurrentPosition == mQuestionsList!!.size){
+            btnSubmit?.text = "Finish"
+        }
+        else{
+            btnSubmit?.text = "Next"
+        }
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+}
