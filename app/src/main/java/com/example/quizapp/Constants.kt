c1f17ad04062580c035ffafdd019d12a3752b0f4
ux-tahmid <tahmid@uxd.co.jp>
2024-01-05 16:32:29 +0600

    final commit

--- app/src/main/java/com/example/quizapp/Constants.kt
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -3,6 +3,9 @@ package com.example.quizapp
 import kotlin.collections.ArrayList
 
 object Constants {
+    const val USER_NAME: String = "user_name"
+    const val TOTAL_QUESTIONS: String = "total_questions"
+    const val CORRECT_ANSWERS: String = "correct_answers"
     fun getQuestions(): ArrayList<Question>{
         val questionsList = ArrayList<Question>()
         val que1 = Question(
